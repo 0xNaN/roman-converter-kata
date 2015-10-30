482eb830e8cfd1e1c3c377690aa19e4540aa6868
0xNaN <enzodicicco@gmail.com>
2015-10-30 10:35:55 +0100

    refactoring test. Adding parameterized tests

---
+++ tests/RomanConverterParameterizedTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class RomanConverterParameterizedTest {
+	private RomanConverter romanConverter;
+	private int arabic;
+	private String romanExpected;
+
+	
+	public RomanConverterParameterizedTest(int arabic, String roman) {
+		this.arabic = arabic;
+		this.romanExpected = roman;
+	}
+	
+	@Before
+	public void setUp() {
+		this.romanConverter = new RomanConverter();
+	}
+
+	@Parameters
+	public static Collection<Object[]> testCases () {
+		return Arrays.asList(new Object[][] {
+				{1, "I"},
+				{5, "V"},
+				{10, "X"}
+		});
+	}
+	
+	@Test
+	public void checkAll() {
+		assertEquals(romanExpected, romanConverter.convert(arabic));
+	}
+}
--- tests/RomanConverterTest.java
+++ tests/RomanConverterTest.java
@@ -17,22 +17,4 @@ public class RomanConverterTest {
 	should_return_empty_string_for_zero() throws Exception {
 		assertEquals("", romanConverter.convert(0));
 	}
-	
-	@Test
-	public void 
-	should_return_I_for_one() throws Exception {
-		assertEquals("I", romanConverter.convert(1));
-	}
-	
-	@Test
-	public void
-	should_return_V_for_five() throws Exception {
-		assertEquals("V", romanConverter.convert(5));
-	}
-	
-	@Test
-	public void
-	should_return_X_for_ten() throws Exception {
-		assertEquals("X", romanConverter.convert(10));
-	}
 }
