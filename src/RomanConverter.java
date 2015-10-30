9731155bcdf4d9a731b506f6fd639d63af73ef24
0xNaN <enzodicicco@gmail.com>
2015-10-30 11:06:29 +0100

    extracting known romans

--- src/RomanConverter.java
+++ src/RomanConverter.java
@@ -1,18 +1,20 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class RomanConverter {
+	Map<Integer, String> arabic2romans = new LinkedHashMap<Integer, String>() {{
+		put(1,	"I");
+		put(5,  "V");
+		put(10, "X");
+	}};
 	
 	public String convert(int arabic) {
 		if(arabic == 0)
 			return "";
-		/*
-		 * these lines seems a simple look up
-		 * in a table
-		 */
-		else if(arabic == 1)
-			return "I";
-		else if(arabic == 5)
-			return "V";
-		else if(arabic == 10)
-			return "X";
+		
+		if(arabic2romans.containsKey(arabic))
+			return arabic2romans.get(arabic);
+		
 		int factor = biggerKnowRomansLesserThan(arabic);
 		return convert(factor)+convert(arabic - factor);
 	}
