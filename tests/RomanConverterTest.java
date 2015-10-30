d348794d0c8e7962b7b54eb0efdbbee4b2433813
0xNaN <enzodicicco@gmail.com>
2015-10-30 10:02:24 +0100

    refactoring tests

--- tests/RomanConverterTest.java
+++ tests/RomanConverterTest.java
@@ -1,21 +1,26 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class RomanConverterTest {
+	RomanConverter romanConverter;
+	
+	@Before
+	public void setUp() {
+		romanConverter = new RomanConverter();
+	}
 	
 	@Test
 	public void 
 	should_return_empty_string_for_zero() throws Exception {
-		RomanConverter romanConverter = new RomanConverter();
 		assertEquals("", romanConverter.convert(0));
 	}
 	
 	@Test
 	public void 
 	should_return_I_for_one() throws Exception {
-		RomanConverter romanConverter = new RomanConverter();
 		assertEquals("I", romanConverter.convert(1));
 	}
 }
